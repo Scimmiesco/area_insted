60ed782ff36ee93926e925b5ae7ef49ce9fc760e
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-05-24 23:43:32 -0300

    .

--- ava/src/app/app.module.ts
+++ ava/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
@@ -10,7 +10,6 @@ import { LoadingComponent } from './components/loading/loading.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { HomeModule } from './home/home.module';
-import { CarouselModule } from './components/carousel/carousel.module';
 import { register } from 'swiper/element/bundle';
 
 register();
