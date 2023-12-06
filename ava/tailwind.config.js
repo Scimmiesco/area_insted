59cf281c7252f1a5f33efbb442cfdcf0c5b1f060
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2023-12-06 08:34:50 -0400

    Ajustes em cores

--- ava/tailwind.config.js
+++ ava/tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
       okay_button: "rgb(22, 129, 193)",
       green_insted: "#1da9ad",
       green_insted_dark: "#063b3d",
+      green_insted_dark_sec:"#104D50",
       green_insted_transp_dark: "rgba(35, 111, 114, 0.35)",
       green_insted_transp: "rgba(103, 219, 223, 0.5)",
       green_insted_fundo_modal: "rgba(186, 234, 236, 0.384)",
