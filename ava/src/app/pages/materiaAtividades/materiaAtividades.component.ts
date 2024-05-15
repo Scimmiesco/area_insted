95ad3570bfb573ac6692dd7e213ec211836c51ae
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2024-05-14 23:24:58 -0400

    ajustesvisual materias

--- ava/src/app/pages/materiaAtividades/materiaAtividades.component.ts
+++ ava/src/app/pages/materiaAtividades/materiaAtividades.component.ts
@@ -1,28 +1,36 @@
-import { Router, RouterModule, ActivatedRoute } from '@angular/router';
-import {Component, NgModule} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MateriasService } from 'app/services/materias.service';
 
 @Component({
   selector: 'app-materia',
   templateUrl: './materiaAtividades.component.html',
 })
 export class MateriaAtividadesComponent {
+  idMateria: number = 0;
+  private subscription!: Subscription;
 
   constructor(
     public router: Router,
-    public activatedRoute : ActivatedRoute)
-  {
-
-    this.getIDMateria().subscribe((value)=>{
-
+    public activatedRoute: ActivatedRoute,
+    public materiasService: MateriasService
+  ) {
+    this.subscription = this.getIDMateria().subscribe((value) => {
+      this.idMateria = value;
     });
   }
 
-  getIDMateria(): Observable<string> {
-    let idMateria : Observable<string>;
-    idMateria = this.activatedRoute.params.pipe(map((p) => p['id']));
-    return idMateria;
+  getIDMateria(): Observable<number> {
+    return this.activatedRoute.params.pipe(
+      map((params) => Number(params['id']))
+    );
   }
-
+  ngOnDestroy(): void {
+    
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
