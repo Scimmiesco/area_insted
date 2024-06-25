9096cbf9306d97ec5d05016781c85e6859517987
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2024-06-25 11:57:12 -0400

    adicionado no tipo arquivo

--- ava/src/app/components/atividades/arquivo/arquivo.component.ts
+++ ava/src/app/components/atividades/arquivo/arquivo.component.ts
@@ -3,6 +3,7 @@ import {
   IAtividadeArquivo,
   TiposAtividades,
 } from 'app/Interfaces/atividade.interface';
+import { AtividadesService } from 'app/services/atividades.service';
 
 @Component({
   selector: 'app-arquivo',
@@ -12,15 +13,39 @@ import {
 export class ArquivoComponent {
   @Input() atividadeArquivo!: IAtividadeArquivo;
 
-  atividadeVista!: boolean;
+  atividadeVista = false as boolean;
   EnumTiposAtividades = TiposAtividades;
+  arquivoAdicionado!: boolean;
+  fileName!: string;
+  caminhoArquivo!: string;
 
-  constructor() {
-    this.atividadeVista = false;
-  }
+  constructor(public atividadesService: AtividadesService) {}
 
   ngOnInit(): void {}
   marcarVisto() {
     this.atividadeVista = !this.atividadeVista;
+  }
+  anexarArquivo(e: Event) {
+    const input = e.target as HTMLInputElement;
+
+    this.arquivoAdicionado = input?.files ? true : false;
+    input?.files != null
+      ? (this.fileName = input?.files[0].name)
+      : (this.fileName = 'Nenhum arquivo adicionado');
+    if (input?.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.fileName = file.name;
+      this.atividadesService.upload(file).subscribe(
+        (response) => {
+          this.caminhoArquivo = response.filePath;
+          this.fileName = 'Arquivo carregado com sucesso!';
+        },
+        (error) => {
+          this.fileName = 'Erro ao carregar o arquivo.';
+        }
+      );
+    } else {
+      this.fileName = 'Nenhum arquivo adicionado';
+    }
   }
 }
